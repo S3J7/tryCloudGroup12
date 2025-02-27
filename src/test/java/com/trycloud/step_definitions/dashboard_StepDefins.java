66b5f195196d3068c0fd569d80129554e1cbd6fe
mahmutekmekci <mahmute3541@gamil.com>
2025-02-26 23:12:53 -0500

    Merge remote-tracking branch 'origin/develop' into mahmud

--- src/test/java/com/trycloud/step_definitions/dashboard_StepDefins.java
+++ src/test/java/com/trycloud/step_definitions/dashboard_StepDefins.java
@@ -15,7 +15,7 @@ import org.openqa.selenium.WebElement;
 import java.util.Arrays;
 import java.util.List;
 
-public class dashboard_StepDefs {
+public class dashboard_StepDefins {
 
     LoginPage loginPage = new LoginPage();
     DashboardPage dashboardPage = new DashboardPage();
